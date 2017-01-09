c2b43ba61b9069d71ce601203a22ce99cd6c5115
ws_binz <binz@wicresof.com>
2017-01-09 19:27:32 +0800

    binz

--- BinzApp/android/app/src/main/java/com/binzapp/baiduMap/BaiduMapViewManager.java
+++ BinzApp/android/app/src/main/java/com/binzapp/baiduMap/BaiduMapViewManager.java
@@ -3,10 +3,16 @@ package com.binzapp.baiduMap;
 import android.content.Context;
 
 import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.model.LatLng;
 import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewGroupManager;
+import com.facebook.react.uimanager.annotations.ReactProp;
 
 /**
  * Created by binz on 2017/1/4.
@@ -28,8 +34,77 @@ public class BaiduMapViewManager extends ViewGroupManager<MapView> {
 
     public MapView createViewInstance(ThemedReactContext context) {
         mReactContext = context;
-        MapView mapView =  new MapView(context);
+        MapView mapView = new MapView(context);
+        mapView.setCustomMapStylePath("custom_config");
         return mapView;
     }
+
+    /**
+     * 地图类型：普通 - MAP_TYPE_NORMAL
+     * 卫星 - MAP_TYPE_SATELLITE
+     * 空白 - MAP_TYPE_NON
+     *
+     * @param mapView
+     * @param mapType
+     */
+    @ReactProp(name = "mapType")
+    public void setMapType(MapView mapView, int mapType) {
+        mapView.getMap().setMapType(mapType);
+    }
+
+    /**
+     * 地图比例尺：3 - 21
+     *
+     * @param mapView
+     * @param zoom
+     */
+    @ReactProp(name = "zoom")
+    public void setZoom(MapView mapView, float zoom) {
+        MapStatus mapStatus = new MapStatus.Builder().zoom(zoom).build();
+        MapStatusUpdate mapStatusUpdate = MapStatusUpdateFactory.newMapStatus(mapStatus);
+        mapView.getMap().setMapStatus(mapStatusUpdate);
+    }
+
+
+    /**
+     * 设置地图最大以及最小缩放级别,地图支持的最大最小级别分别为[3-21]
+     *
+     * @param mapView
+     * @param maxAndMinZoom
+     */
+    @ReactProp(name = "maxAndMinZoom")
+    public void setMaxAndMinZoom(MapView mapView, ReadableArray maxAndMinZoom) {
+        // 默认地图缩放最大，最小等级
+        float minLevel = 3f;
+        float maxLevel = 21f;
+        if (!maxAndMinZoom.isNull(0)) {
+            minLevel = maxAndMinZoom.getInt(0);
+        }
+        if (!maxAndMinZoom.isNull(1)) {
+            maxLevel = maxAndMinZoom.getInt(1);
+        }
+        mapView.getMap().setMaxAndMinZoomLevel(maxLevel, minLevel);
+    }
+
+    /**
+     * 设置地图中心
+     *
+     * @param mapView
+     * @param position
+     */
+    @ReactProp(name = "center")
+    public void setCenter(MapView mapView, ReadableMap position) {
+        if (position != null) {
+            double latitude = position.getDouble("latitude");
+            double longitude = position.getDouble("longitude");
+            LatLng point = new LatLng(latitude, longitude);
+            MapStatus mapStatus = new MapStatus.Builder()
+                    .target(point)
+                    .build();
+            MapStatusUpdate mapStatusUpdate = MapStatusUpdateFactory.newMapStatus(mapStatus);
+            mapView.getMap().setMapStatus(mapStatusUpdate);
+        }
+    }
+
 
 }
--- BinzApp/js/BaiduMap/MapView.js
+++ BinzApp/js/BaiduMap/MapView.js
@@ -11,16 +11,40 @@ import React, {
   PropTypes
 } from 'react';
 
+// 导入常量设定 - MapTypes
+import MapTypes from './MapTypes';
+
 export default class MapView extends Component {
+  /**
+  * 定义参数propTypes - 名字:类型
+  */
   static propTypes = {
-    ...View.propTypes
+    ...View.propTypes,
+    mapType: PropTypes.number,
+    zoom: PropTypes.number,
+    center: PropTypes.object,
+    maxAndMinZoom: PropTypes.array
   };
+
+  /**
+  * 初始化参数
+  */
+  static defaultProps = {
+    mapType: MapTypes.NORMAL,
+    zoom: 15,
+    center: {
+              'longitude': 121.667917,
+              'latitude': 31.149712
+            },
+    maxAndMinZoom: [14, 21]
+  }
 
   constructor() {
     super();
   }
 
   render() {
+    // 初始化地图组件
     return <BaiduMapView {...this.props}/>;
   }
 }
